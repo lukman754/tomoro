f669c58335938fedd3f651cbde291571e37857cc
Lukman Muludin <43158553+lukman754@users.noreply.github.com>
2024-10-06 18:53:25 +0700

    Add files via upload

---
+++ product.js
@@ -0,0 +1,162 @@
+const products = [{
+    id: 1,
+    name: "Cheese Cloud Chocolate",
+    price: 15000,
+    originalPrice: 26000, // Harga asli yang dicoret
+    category: "cheese",
+    image: "https://s3.ap-northeast-1-ntt.wasabisys.com/ak1520d-filenow-8/files/20241005-1910_51e92db28e6cd1bc711bd3100b0d9d51.jpg"
+  },
+  {
+    id: 2,
+    name: "Caramel Cheese Latte",
+    price: 15000,
+    originalPrice: 26000, // Harga asli sama, tidak dicoret
+    category: "cheese",
+    image: "https://s3.ap-northeast-1-ntt.wasabisys.com/ak1520d-filenow-2/files/20241006-1646_c0dc53da26a601b442b409626fc3b841.jpg"
+  },
+  {
+    id: 3,
+    name: "Cheese Cloud Latte",
+    price: 15000,
+    originalPrice: 26000,
+    category: "cheese",
+    image: "https://s3.ap-northeast-1-ntt.wasabisys.com/ak1520d-filenow-2/files/20241006-1646_c0dc53da26a601b442b409626fc3b841.jpg"
+  },
+  {
+    id: 4,
+    name: "Coconut Choco",
+    price: 15000,
+    originalPrice: 22000,
+    category: "coconut",
+    image: "#"
+  },
+  {
+    id: 5,
+    name: "Coconut Aren Frappe",
+    price: 15000,
+    originalPrice: 25000,
+    category: "coconut",
+    image: "#"
+  },
+  {
+    id: 6,
+    name: "TOMORO Coconut Latte",
+    price: 15000,
+    originalPrice: 22000,
+    category: "coconut",
+    image: "#"
+  },
+  {
+    id: 7,
+    name: "Coconut Aren Latte",
+    price: 15000,
+    originalPrice: 24000,
+    category: "coconut",
+    image: "#"
+  },
+  {
+    id: 8,
+    name: "Sea Salt Cloud Matcha Latte",
+    price: 15000,
+    originalPrice: 28000,
+    category: "cloud",
+    image: "#"
+  },
+  {
+    id: 9,
+    name: "Sea Salt Cloud Caramel Latte",
+    price: 15000,
+    originalPrice: 28000,
+    category: "cloud",
+    image: "#"
+  },
+  {
+    id: 10,
+    name: "Sea Salt Cloud Chocolate",
+    price: 15000,
+    originalPrice: 25000,
+    category: "cloud",
+    image: "#"
+  },
+  {
+    id: 11,
+    name: "TOMORO Aren Latte",
+    price: 15000,
+    originalPrice: 20000,
+    category: "coffee",
+    image: "#"
+  },
+  {
+    id: 12,
+    name: "TOMORO Oat Latte",
+    price: 15000,
+    originalPrice: 24000,
+    category: "coffee",
+    image: "#"
+  },
+  {
+    id: 13,
+    name: "Manuka Oat Latte",
+    price: 15000,
+    originalPrice: 30000,
+    category: "coffee",
+    image: "#"
+  },
+  {
+    id: 14,
+    name: "Caffe Latte",
+    price: 15000,
+    originalPrice: 24000,
+    category: "coffee",
+    image: "#"
+  },
+  {
+    id: 15,
+    name: "Caffe Americano",
+    price: 15000,
+    originalPrice: 18000,
+    category: "coffee",
+    image: "#"
+  },
+  {
+    id: 16,
+    name: "Breve Latte",
+    price: 15000,
+    originalPrice: 30000,
+    category: "coffee",
+    image: "#"
+  },
+  {
+    id: 17,
+    name: "Cappuccino",
+    price: 15000,
+    originalPrice: 24000,
+    category: "coffee",
+    image: "#"
+  },
+  {
+    id: 18,
+    name: "Caffe Mocha",
+    price: 15000,
+    originalPrice: 25000,
+    category: "coffee",
+    image: "#"
+  },
+  {
+    id: 19,
+    name: "Flat White",
+    price: 15000,
+    originalPrice: 24000,
+    category: "coffee",
+    image: "#"
+  },
+  {
+    id: 20,
+    name: "Caramel Macchiato",
+    price: 15000,
+    originalPrice: 25000,
+    category: "coffee",
+    image: "#"
+  }
+
+];
---
+++ script.js
@@ -0,0 +1,183 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    let cart = [];
+
+    // Fungsi untuk memformat angka ke dalam format Rupiah
+    function formatRupiah(amount) {
+        return amount.toLocaleString("id-ID", {
+            style: "currency",
+            currency: "IDR",
+            minimumFractionDigits: 0
+        });
+    }
+
+    function displayProducts(filter = "*") {
+        const productList = document.getElementById("product-list");
+        productList.innerHTML = "";
+
+        const categories = [...new Set(products.map(product => product.category))];
+
+        categories.forEach(category => {
+            const categoryHeader = document.createElement("h2");
+            categoryHeader.innerText = category.charAt(0).toUpperCase() + category.slice(1);
+            productList.appendChild(categoryHeader);
+
+            const filteredProducts = products.filter(product => {
+                return product.category === category && (filter === "*" || product.category === filter.replace(".", ""));
+            });
+
+            const categoryContainer = document.createElement("div");
+            categoryContainer.classList.add("category-container");
+
+            filteredProducts.forEach(product => {
+                const productElement = document.createElement("div");
+                productElement.classList.add("product", product.category);
+
+                // Tentukan apakah harga asli dan harga diskon ditampilkan
+                const priceDisplay = product.price !== product.originalPrice ?
+                    `<p class="fs-6 text-danger"><span style="text-decoration: line-through; color: gray;">${formatRupiah(product.originalPrice)}</span> ${formatRupiah(product.price)}</p>` :
+                    `<p class="fs-6 text-danger">${formatRupiah(product.price)}</p>`;
+
+                productElement.innerHTML = `
+                <div class="row align-items-center mb-3">
+                    <div class="col-4 col-lg-2">
+                        <img src="${product.image}" alt="${product.name}" class="product-image" style="width: 80px; height: 80px" />
+                    </div>
+                    <div class="col-6 col-lg-8">
+                        <h5 class="fs-5">${product.name}</h5>
+                        ${priceDisplay}
+                    </div>
+                    <div class="col-2 d-flex justify-content-end align-items-end">
+                        <button class="btn btn-primary rounded-pill btn-sm" data-id="${product.id}">
+                            <i class="fas fa-plus"></i>
+                        </button>
+                    </div>
+                </div>
+            `;
+                categoryContainer.appendChild(productElement);
+            });
+
+            productList.appendChild(categoryContainer);
+        });
+
+        const buttons = document.querySelectorAll(".product button");
+        buttons.forEach(button => {
+            button.addEventListener("click", function () {
+                const productId = parseInt(this.getAttribute("data-id"));
+                addToCart(productId);
+            });
+        });
+    }
+
+
+
+    function addToCart(productId) {
+        const product = products.find(p => p.id === productId);
+        const cartItem = cart.find(item => item.id === productId);
+
+        if (cartItem) {
+            cartItem.quantity += 1;
+        } else {
+            cart.push({
+                ...product,
+                quantity: 1
+            });
+        }
+
+        updateCart();
+    }
+
+    function updateCart() {
+        const cartDiv = document.getElementById("cart");
+        cartDiv.innerHTML = "";
+        let total = 0;
+        let totalQuantity = 0;
+
+        cart.forEach((item, index) => {
+            const itemTotal = item.price * item.quantity;
+            total += itemTotal;
+            totalQuantity += item.quantity;
+            cartDiv.innerHTML += `
+                    <div class='row'>
+                        <div class='col-7'>${item.name} </div>
+                        <div class='col-1'>${item.quantity}x</div>
+                        <div class='col-2'>${formatRupiah(itemTotal)}</div>
+                        <div class='col-2 d-flex justify-content-end'>
+                            <a class="remove-item text-danger rounded-pill btn-sm" data-index="${index}">
+                                <i class="fas fa-times"></i>
+                            </a>
+                        </div>
+                    </div>`;
+        });
+
+        document.getElementById("total").innerText = formatRupiah(total);
+
+        // Update jumlah produk di bubble
+        document.getElementById("cart-count").innerText = totalQuantity;
+
+        // Tambahkan event listener untuk tombol hapus
+        const removeButtons = document.querySelectorAll(".remove-item");
+        removeButtons.forEach(button => {
+            button.addEventListener("click", function () {
+                const index = parseInt(this.getAttribute("data-index"));
+                removeFromCart(index);
+            });
+        });
+    }
+
+    function removeFromCart(index) {
+        cart.splice(index, 1); // Hapus item dari keranjang
+        updateCart(); // Perbarui tampilan keranjang
+    }
+
+    document.getElementById("search").addEventListener("input", function () {
+        const query = this.value.toLowerCase();
+        const productList = document.querySelectorAll(".product");
+
+        productList.forEach(product => {
+            const isVisible = product.innerText.toLowerCase().includes(query);
+            product.style.display = isVisible ? "block" : "none";
+        });
+    });
+
+    const filterButtons = document.querySelectorAll(".filters button");
+    filterButtons.forEach(button => {
+        button.addEventListener("click", function () {
+            const filterValue = this.getAttribute("data-filter");
+            displayProducts(filterValue);
+        });
+    });
+
+    function sendWhatsAppMessage(name, location) {
+        const message = cart.map(item => `${item.name} - ${item.quantity}x`).join('%0A');
+        const total = cart.reduce((sum, item) => sum + (item.price * item.quantity), 0);
+        const finalMessage = `Nama: ${name}%0ALokasi Tomoro: ${location}%0A%0A${message}%0A%0ATotal: ${formatRupiah(total)}`;
+        const phoneNumber = "6285156477250"; // Ganti dengan nomor WhatsApp yang dituju
+        const url = `https://wa.me/${phoneNumber}?text=${finalMessage}`;
+        window.open(url, '_blank');
+    }
+
+    document.getElementById("order-form").addEventListener("submit", function (event) {
+        event.preventDefault(); // Mencegah pengiriman form
+        const name = document.getElementById("name").value;
+        const location = document.getElementById("location").value;
+
+        // Mengirim pesan ke WhatsApp
+        sendWhatsAppMessage(name, location);
+
+        // Mengosongkan keranjang tetapi tidak mereset input name dan location
+        cart = [];
+        updateCart();
+
+        // Reset form kecuali input name dan location
+        document.querySelectorAll("#order-form input").forEach(input => {
+            if (input.id !== "name" && input.id !== "location") {
+                input.value = '';
+            }
+        });
+    });
+
+
+    // Menampilkan semua produk saat halaman dimuat
+    displayProducts();
+});
