97010cac5c8d566134b9a3ce69842eedf5fbc269
Lukman Muludin <43158553+lukman754@users.noreply.github.com>
2024-10-07 10:07:43 +0700

    Update product.js

--- product.js
+++ product.js
@@ -2,7 +2,7 @@ const products = [{
     id: 1,
     name: "Cheese Cloud Chocolate",
     price: 15000,
-    originalPrice: 26000, // Harga asli yang dicoret
+    originalPrice: 26000,
     category: "cheese",
     image: "https://s3.ap-northeast-1-ntt.wasabisys.com/ak1520d-filenow-8/files/20241005-1910_51e92db28e6cd1bc711bd3100b0d9d51.jpg"
   },
@@ -10,7 +10,7 @@ const products = [{
     id: 2,
     name: "Caramel Cheese Latte",
     price: 15000,
-    originalPrice: 26000, // Harga asli sama, tidak dicoret
+    originalPrice: 26000,
     category: "cheese",
     image: "https://s3.ap-northeast-1-ntt.wasabisys.com/ak1520d-filenow-2/files/20241006-1646_c0dc53da26a601b442b409626fc3b841.jpg"
   },
@@ -22,6 +22,7 @@ const products = [{
     category: "cheese",
     image: "https://s3.ap-northeast-1-ntt.wasabisys.com/ak1520d-filenow-2/files/20241006-1646_c0dc53da26a601b442b409626fc3b841.jpg"
   },
+
   {
     id: 4,
     name: "Coconut Choco",
@@ -54,6 +55,7 @@ const products = [{
     category: "coconut",
     image: "https://s3.ap-northeast-1-ntt.wasabisys.com/ak1520d-filenow-2/files/20241006-2218_13740ce222dd520a1cda40117865c814.jpg"
   },
+
   {
     id: 8,
     name: "Sea Salt Cloud Matcha Latte",
@@ -78,6 +80,7 @@ const products = [{
     category: "cloud",
     image: "https://s3.ap-northeast-1-ntt.wasabisys.com/ak1520d-filenow-11/files/20241006-2220_e58e87f82fe774c51bc3121e9e08979d.png"
   },
+
   {
     id: 11,
     name: "TOMORO Aren Latte",
@@ -157,6 +160,151 @@ const products = [{
     originalPrice: 25000,
     category: "coffee",
     image: "https://s3.ap-northeast-1-ntt.wasabisys.com/ak1520d-filenow-11/files/20241006-2150_d8f4c19159b2192145a0b17c42411de5.jpg"
-  }
+  },
 
+  {
+    id: 21,
+    name: "Coconut Aren Frappe",
+    price: 15000,
+    originalPrice: 25000,
+    category: "frappe",
+    image: "https://i.im.ge/2024/10/07/k0rGW8.4-20241007-094141-0000.png"
+  },
+  {
+    id: 22,
+    name: "Oat Aren Frappe",
+    price: 15000,
+    originalPrice: 27000,
+    category: "frappe",
+    image: "https://i.im.ge/2024/10/07/k0rvyF.5-20241007-094141-0001.png"
+  },
+  {
+    id: 23,
+    name: "Coffee Frappe",
+    price: 15000,
+    originalPrice: 24000,
+    category: "frappe",
+    image: "https://i.im.ge/2024/10/07/k0r4O9.6-20241007-094141-0002.png"
+  },
+  {
+    id: 24,
+    name: "Aren Latte Frappe",
+    price: 15000,
+    originalPrice: 25000,
+    category: "frappe",
+    image: "https://i.im.ge/2024/10/07/k0rBmX.7-20241007-094142-0003.png"
+  },
+  {
+    id: 25,
+    name: "Matcha Frappe",
+    price: 15000,
+    originalPrice: 27000,
+    category: "frappe",
+    image: "https://i.im.ge/2024/10/07/k0rJzK.8-20241007-094142-0004.png"
+  },
+  {
+    id: 26,
+    name: "Chocolate Frappe",
+    price: 15000,
+    originalPrice: 24000,
+    category: "frappe",
+    image: "https://i.im.ge/2024/10/07/k0rps6.9-20241007-094142-0005.png"
+  },
+  {
+    id: 27,
+    name: "Caffe Mocha Frappe",
+    price: 15000,
+    originalPrice: 25000,
+    category: "frappe",
+    image: "https://i.im.ge/2024/10/07/k0rps6.9-20241007-094142-0005.png"
+  },
+
+  {
+    id: 28,
+    name: "Coconut Choco",
+    price: 15000,
+    originalPrice: 22000,
+    category: "noncoffee",
+    image: "https://s3.ap-northeast-1-ntt.wasabisys.com/ak1520d-filenow-5/files/20241006-2215_d5fba1ad042c05da833c58e5ea595ecf.jpg"
+  },
+  {
+    id: 29,
+    name: "Matcha Oat Latte",
+    price: 15000,
+    originalPrice: 28000,
+    category: "noncoffee",
+    image: "https://i.im.ge/2024/10/06/ki4ipJ.1-20241006-225418-0000.png"
+  },
+  {
+    id: 30,
+    name: "Choco Oat Latte",
+    price: 15000,
+    originalPrice: 28000,
+    category: "noncoffee",
+    image: "https://i.im.ge/2024/10/06/ki4hCa.2-20241006-225418-0001.png"
+  },
+  {
+    id: 31,
+    name: "Pink Pop Lemonade",
+    price: 15000,
+    originalPrice: 14000,
+    category: "noncoffee",
+    image: "https://i.im.ge/2024/10/06/ki4S2r.3-20241006-225418-0002.png"
+  },
+  {
+    id: 32,
+    name: "Pink Pop Lemon Tea",
+    price: 15000,
+    originalPrice: 17000,
+    category: "noncoffee",
+    image: "https://i.im.ge/2024/10/06/ki41pm.4-20241006-225418-0003.png"
+  },
+  {
+    id: 33,
+    name: "Hojicha Oat Latte",
+    price: 15000,
+    originalPrice: 30000,
+    category: "noncoffee",
+    image: "https://i.im.ge/2024/10/06/ki452y.5-20241006-225418-0004.png"
+  },
+  {
+    id: 34,
+    name: "Chocolate",
+    price: 15000,
+    originalPrice: 21000,
+    category: "noncoffee",
+    image: "https://i.im.ge/2024/10/06/ki4dwW.6-20241006-225418-0005.png"
+  },
+  {
+    id: 35,
+    name: "Matcha Latte",
+    price: 15000,
+    originalPrice: 27000,
+    category: "noncoffee",
+    image: "https://i.im.ge/2024/10/06/ki4qK0.7-20241006-225418-0006.png"
+  },
+  {
+    id: 36,
+    name: "Jasmine Green Tea",
+    price: 15000,
+    originalPrice: 17000,
+    category: "noncoffee",
+    image: "https://i.im.ge/2024/10/07/k0raPK.1-20241007-093420-0000.png"
+  },
+  {
+    id: 37,
+    name: "Earl Grey Tea",
+    price: 15000,
+    originalPrice: 17000,
+    category: "noncoffee",
+    image: "https://i.im.ge/2024/10/07/k0rIFX.2-20241007-093420-0001.png"
+  },
+  {
+    id: 38,
+    name: "Pure Chamomile",
+    price: 15000,
+    originalPrice: 17000,
+    category: "noncoffee",
+    image: "https://i.im.ge/2024/10/07/k0r7k9.3-20241007-093420-0002.png"
+  }
 ];
